9bd6dd8172ce06a2419807168dc1cb7b74deee20
Akif Emre Reis <akifemre611@hotmail.com>
2023-09-29 17:58:28 +0300

    cv.py iframe embed tagi ile değiştirildi

--- personal_website/pages/📜CV.py
+++ personal_website/pages/📜CV.py
@@ -7,7 +7,7 @@ def displayPDF(file):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1100" height="1000" type="application/pdf"></iframe>'
+    pdf_display = pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width=1000 height="1000" type="application/pdf">'
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
